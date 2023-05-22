545f5c6783c8209f76a4a5fee3275f671ecbff87
Md Rifat Hossen <rifatsaown0@gmail.com>
2023-05-22 14:09:07 +0600

    Project Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Footer from "./pages/Shared/Footer/Footer";
+import Navbar from "./pages/Shared/Navbar/Navbar";
 
 function App() {
   return (
     <>
-      <Outlet></Outlet>
+    <Navbar/>
+      <Outlet/>
+      <Footer/>
     </>
   );
 }
