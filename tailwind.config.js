393e04cf95584d2f807054a8ae59d208b1b86ef7
Md Rifat Hossen <rifatsaown0@gmail.com>
2023-05-22 13:57:35 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  // Chose the theme you want to enable.
+  // Remove the line if you want to use the default theme
+  // full docs here: https://daisyui.com/docs/themes/
+  daisyui: {
+    themes: ["emerald"],
+  },
+}
+
