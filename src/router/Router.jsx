862c2611f987e1eeb4c63e522d6ddca025dece69
Md Rifat Hossen <rifatsaown0@gmail.com>
2023-06-05 04:03:26 +0600

    error route build

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,10 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import Bookings from "../pages/Bookings/Bookings";
 import Checkout from "../pages/Checkout/Checkout";
 import Home from "../pages/Home/Home/Home";
 import Login from "../pages/Login/Login";
 import SignUp from "../pages/SignUP/SignUp";
-import Bookings from "../pages/Bookings/Bookings";
 import PrivateRoute from "./PrivateRoute";
 
 const router = createBrowserRouter([
@@ -18,13 +18,28 @@ const router = createBrowserRouter([
       { path: "/SignUp", element: <SignUp /> },
       {
         path: "checkout/:id",
-        element: <PrivateRoute><Checkout/></PrivateRoute>,
-        loader: ({params}) => fetch(`http://localhost:5000/services/${params.id}`),
+        element: (
+          <PrivateRoute>
+            <Checkout />
+          </PrivateRoute>
+        ),
+        loader: ({ params }) =>
+          fetch(`http://localhost:5000/services/${params.id}`),
       },
       {
         path: "/bookings",
-        element: <PrivateRoute><Bookings/></PrivateRoute>,
-      }
+        element: (
+          <PrivateRoute>
+            <Bookings />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "*",
+        element: (
+          <h1 className="text-red-800 font-extrabold text-center m-8 text-5xl">No Data Found</h1>
+        ),
+      },
     ],
   },
 ]);
